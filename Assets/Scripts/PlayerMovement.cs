2eedbb22cd1f770f03eb3873b7fac2032977453a
Jelle-Laptop\Jelle <jejong1@gmail.com>
2018-01-27 14:57:16 +0100

    Fuckboi fixed the shit ouwe

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,23 +13,16 @@ public class PlayerMovement : MonoBehaviour {
 
     public States state;
 
-    public float speed;
-    public float maxVelocity;
-    [Range(0, 1)]
-    public float drag;
-    public float jumpForce;
-    public float wallJumpForce;
-    public float wallJumpSidewaysForce;
-    public float wallSlideSpeed;
+    public float speed = 14f;
+    public float jumpForce = 14f;
+	public float accel = 6f;
+	public float airAccel = 3f;
 
-    public float dashVelocity;
+	public float dashVelocity;
     public float dashCooldown;
     public float dashTimeStamp;
 
     public float stunTime;
-
-    public bool grounded = false;
-    public bool onWall = false;
     public bool canMove = true;
 
     Rigidbody2D rb;
@@ -43,6 +36,7 @@ public class PlayerMovement : MonoBehaviour {
     float h;
 
     private GroundState groundState;
+	private Vector3 input; // z is dash 
 
     void Start ()
     {
@@ -53,183 +47,38 @@ public class PlayerMovement : MonoBehaviour {
         groundState = new GroundState(transform.gameObject);
     }	
 
-	void FixedUpdate ()
+	void Update ()
     {
-        h = Input.GetAxis("Horizontal");
+        input.x = Input.GetAxis("Horizontal");
+		
 
-        if (canMove)
-        {
-            Movement();
-            Dash();
-        }
+		if (Input.GetButton("A"))
+			input.y = 1;
+
+		if(Input.GetButtonDown("X"))
+		{
+			input.z = 1;
+		}
 	}
 
-    void Dash()
+
+
+	void FixedUpdate()
     {
-        switch (state)
-        {
-            case States.Ready:
+		Dash();
 
-                if (Input.GetButtonDown("X") && h != 0)
-                {
-                    state = States.Dashing;
-                }
-                break;
+		rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * (groundState.isGround() ? accel : airAccel), 0)); //Move player.
+		rb.velocity = new Vector2((input.x == 0 && groundState.isGround()) ? 0 : rb.velocity.x,
+								(input.y == 1 && groundState.isTouching()) ? jumpForce : rb.velocity.y); //Stop player if input.x is 0 (and grounded) and jump if input.y is 1
 
-            case States.Dashing:
+		if (groundState.isWall() && !groundState.isGround() && input.y == 1)
+			rb.velocity = new Vector2(-groundState.wallDirection() * speed * 0.75f, rb.velocity.y); //Add force negative to wall direction (with speed reduction)
 
-                StartCoroutine(PlayerDash());
-                dashTimeStamp = Time.time + dashCooldown;
-                state = States.Cooldown;
-                break;
-
-            case States.Cooldown:
-
-                if (dashTimeStamp <= Time.time)
-                {
-                    state = States.Ready;
-                }
-                break;
-        }
-    }
-
-    IEnumerator PlayerDash()
-    {
-        trail.time = 1f;
-
-        rb.velocity = new Vector2(dashVelocity * h, 0);
-        rb.gravityScale = 0;
-        yield return new WaitForSeconds(0.1f);        
-        rb.velocity = Vector2.zero;
-        rb.velocity = new Vector2(maxVelocity * h, 0);
-        rb.gravityScale = 1;
-
-        trail.time = .1f;
-    }
-
-    IEnumerator Stun()
-    {
-        canMove = false;
-        rb.velocity = new Vector2(0, rb.velocity.y);
-        yield return new WaitForSeconds(stunTime);
-        canMove = true;
-    }
-
-    void Movement()
-    {
-        //Player Movement        
-        currentVelocity = rb.velocity;
-
-        //MaxVelocity Settings
-        if (currentVelocity.x >= maxVelocity)
-        {
-            currentVelocity.x = maxVelocity;
-        }
-        if (currentVelocity.x <= -maxVelocity)
-        {
-            currentVelocity.x = -maxVelocity;
-        }
-        if (currentVelocity.y <= -10)
-        {
-            currentVelocity.y = 10;
-        }
-
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            rb.AddForce(new Vector2(h * speed, 0));
-        }
-        else
-        {
-            rb.velocity = new Vector2(rb.velocity.x * drag, rb.velocity.y);
-        }
-
-        //Jumping/Falling
-        if (Input.GetButton("A") && groundState.isGround() && !groundState.isWall())
-        {            
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-        if (rb.velocity.y < 0 && !groundState.isWall())
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallingGrav) * Time.deltaTime;
-        }
-        else if (rb.velocity.y > 0 && !Input.GetButton("A") && !groundState.isWall())
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMulitplier) * Time.deltaTime;
-        }
-
-
-        //RaycastHit2D leftRay = Physics2D.Raycast(transform.position, Vector2.left);
-        //RaycastHit2D rightRay = Physics2D.Raycast(transform.position, Vector2.right);
-
-        //if (leftRay.collider != null && rightRay.collider != null)
-        //{
-        //    float distanceLeft = Mathf.Abs(leftRay.point.x - transform.position.x);
-        //    float distanceRight = Mathf.Abs(rightRay.point.x - transform.position.x);
-
-        //    if (distanceLeft < distanceRight)
-        //    {
-
-        //    }
-        //    else if (distanceLeft > distanceRight)
-        //    {
-        //        wallJumpSidewaysForce *= -1;
-        //    }
-        //}
-
-        //WallJump
-        if (Input.GetButton("A") && !groundState.isGround() && groundState.isWall())
-        {
-            onWall = false;
-
-            //RaycastHit2D leftRay = Physics2D.Raycast(transform.position, Vector2.left);
-            //RaycastHit2D rightRay = Physics2D.Raycast(transform.position, Vector2.right);
-
-            //if (leftRay.collider != null && rightRay.collider != null)
-            //{
-            //    float distanceLeft = Mathf.Abs(leftRay.point.x - transform.position.x);
-            //    float distanceRight = Mathf.Abs(rightRay.point.x - transform.position.x);
-
-            //    if (distanceLeft < distanceRight)
-            //    {
-                    
-            //    }
-            //    else if (distanceLeft > distanceRight)
-            //    {
-            //        wallJumpSidewaysForce *= -1;
-            //    }
-            //}
-            rb.velocity = new Vector2(groundState.wallDirection() * -wallJumpSidewaysForce, wallJumpForce);
-            //rb.AddForce(new Vector2(wallJumpSidewaysForce, wallJumpForce));
-        }
-    }
+		input.y = 0;
+	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Collider2D coll = collision.collider;
-
-        //if (collision.gameObject.CompareTag("Ground"))
-        //{
-        //    grounded = true;
-        //    onWall = false;
-        //}
-        //if (collision.gameObject.CompareTag("Wall"))
-        //{
-        //    if (!grounded)
-        //    {
-        //        Vector2 contactPoint = collision.contacts[0].point;
-        //        Vector2 center = coll.bounds.center;
-        //        if (contactPoint.x > center.x)
-        //        {
-        //            wallJumpSidewaysForce = -20f;
-        //        }
-        //        else if (contactPoint.x < center.x)
-        //        {
-        //            wallJumpSidewaysForce = 20f; 
-        //        }
-        //        onWall = true;
-        //    }
-        //}
-
         if (collision.gameObject.CompareTag("Mine"))
         {
             StartCoroutine(Stun());
@@ -237,15 +86,60 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        //if (collision.gameObject.CompareTag("Wall"))
-        //{
-        //    onWall = false;
-        //}
-    }
+	void Dash()
+	{
+		switch (state)
+		{
+			case States.Ready:
 
-    private void OnTriggerEnter2D(Collider2D collision)
+				if (input.z == 1 && input.x != 0)
+				{
+					state = States.Dashing;
+				}
+				break;
+
+			case States.Dashing:
+
+				StartCoroutine(PlayerDash());
+				dashTimeStamp = Time.time + dashCooldown;
+				state = States.Cooldown;
+				break;
+
+			case States.Cooldown:
+
+				if (dashTimeStamp <= Time.time)
+				{
+					state = States.Ready;
+				}
+				break;
+		}
+		input.z = 0;
+	}
+
+	IEnumerator PlayerDash()
+	{
+		trail.time = 1f;
+
+		rb.velocity = new Vector2(dashVelocity * input.x, 0);
+		rb.gravityScale = 0;
+		yield return new WaitForSeconds(0.1f);
+		rb.velocity = Vector2.zero;
+		rb.velocity = new Vector2(speed * input.x, 0);
+		rb.gravityScale = 3;
+
+		trail.time = .1f;
+	}
+
+	IEnumerator Stun()
+	{
+		canMove = false;
+		rb.velocity = new Vector2(0, rb.velocity.y);
+		yield return new WaitForSeconds(stunTime);
+		canMove = true;
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("SlowField"))
         {
